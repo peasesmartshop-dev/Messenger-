246776898d15e8d6f6c8259e840f70b98333f8f3
peasesmartshop-dev <peasesmartshop@gmail.com>
2026-03-18 06:47:12 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bulochnaya'
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+@app.route('/')
+def index():
+    return "Сервер мессенджера запущен!"
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Новое сообщение: {msg}")
+    emit('message', msg, broadcast=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
+  
